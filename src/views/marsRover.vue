90f220215d6bc086c160f73bf872ba9d99276649
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-12-02 15:56:52 -0500

    Index of manifest is unrelated to earth date. Index of manifest is unrelated to martian sol. Fixed issue by iterating over array to find sol assoc. with image.

--- src/views/marsRover.vue
+++ src/views/marsRover.vue
@@ -1,9 +1,7 @@
 <script setup>
 import { ref, computed } from 'vue';
-// import { addMonths, getMonth, getYear, subMonths } from 'date-fns';
 
-
-const imageNumber = document.getElementById('numberOfImages');
+const earthDaysText = ref('');
 const date = ref(new Date());
 const minDate = computed(() => new Date(2021, 1, 18)); // 02/18/2021 Perseverance Landing Date
 const maxDate = computed(() => new Date()); // Today's date
@@ -11,67 +9,98 @@ const images = ref([]);
 let pageNumber = 1;
 let correctDate = '';
 let numPhotosTaken = 0;
+let targetSol = 0;
 
-async function getPerserveranceImages(date) {
+const getPerserveranceImages = async (date) => {
   const apiUrl = `https://mars-photos.herokuapp.com/api/v1/rovers/Perseverance/photos?earth_date=${date}&page=${pageNumber}`;
-  let resp = await fetch(apiUrl);
-  let data = await resp.json();
+  const resp = await fetch(apiUrl);
+  const data = await resp.json();
   console.log(apiUrl);
   console.log(data.photos);
-  if (data.photos.length === 0) {
-    document.getElementById('moreImages').style.display = 'none';
-    imageNumber.innerText = 'Perseverance didn\'t take any photos on this date. Try another date.';
-  }
-  images.value = data.photos;
-  let sol = data.photos[0].sol;
-  await getTotalNumberOfImages(sol);
-  imageNumber.innerText = `Perseverance took ${data.photos.length} photos on ${correctDate}.`;
-}
+  images.value = data.photos; // Update the images directly
+};
 
-async function getTotalNumberOfImages(solFromImage) {
-  const apiUrl = `https://mars-photos.herokuapp.com/api/v1/manifests/Perseverance`;
-  let resp = await fetch(apiUrl);
-  let data = await resp.json();
-  numPhotosTaken = data.photo_manifest.photos[solFromImage].total_photos;
-  console.log(numPhotosTaken);
-  imageNumber.innerText = `Perseverance took ${numPhotosTaken} photos on ${correctDate}.`;
-}
-
-function dateCorrection(date) {
+const dateCorrection = (date) => {
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
   const year = date.getFullYear().toString();
   const correctedDate = `${year}-${month}-${day}`;
   correctDate = correctedDate;
-  getPerserveranceImages(correctDate);
-}
+};
 
-function handleSearchImages() {
+const getEarthDaysSinceLanding = async () => {
+  console.log(targetSol);
+  const apiUrl = `https://mars-photos.herokuapp.com/api/v1/manifests/Perseverance`;
+  const resp = await fetch(apiUrl);
+  const data = await resp.json();
+  const index = data.photo_manifest.photos.findIndex(item => item.sol === targetSol);
+
+  if (index !== -1) {
+    console.log('found index for sol');
+    numPhotosTaken = data.photo_manifest.photos[index].total_photos;
+    earthDaysText.value = `Perseverance took ${numPhotosTaken} photos on ${correctDate}.`;
+    console.log(earthDaysText.value);
+  } else {
+    console.log('index not found');
+    earthDaysText.value = `Perseverance took 0 photos on ${correctDate}.`;
+  }
+};
+
+const handleSearchImages = async () => {
   dateCorrection(date.value);
-}
+  await getPerserveranceImages(correctDate);
+  if (images.value && images.value.length > 0) {
+    targetSol = images.value[0].sol;
+    await getEarthDaysSinceLanding();
+  } else {
+    console.error('No images found.');
+  }
+};
 
-function handleNextPage() {
+const handleNextPage = () => {
   pageNumber++;
   getPerserveranceImages(correctDate);
-}
+};
 
 </script>
 
 <template main>
   <section class="perseveranceWrapper">
-<div class="searchSection">
-  <div id="datePickerWrapper">
-  <vue-date-picker :min-date="minDate" :max-date="maxDate" dark teleport-center :hide-navigation="['time', 'minutes', 'hours', 'seconds']" id="datePicker" v-model="date" />
-</div>
-  <button class="button-shadow-border button-shadow" @click="handleSearchImages">Search Images</button>
-  <h2 id="numberOfImages">Perseverance took {{ numPhotosTaken }} photos on {{ correctDate }}.</h2>  
-  <div id="images">
-    <img v-for="image in images" :key="image.id" :src="image.img_src" :title="'Taken on sol #' + image.sol" class="perseveranceImage" />
-  </div>
-  <button class="button-shadow-border button-shadow" id="moreImages" @click="handleNextPage" v-if="images.length > 0">More Images</button>
-</div>
-</section>
-
+    <div class="searchSection">
+      <div id="datePickerWrapper">
+        <vue-date-picker
+          :min-date="minDate"
+          :max-date="maxDate"
+          dark
+          teleport-center
+          :hide-navigation="['time', 'minutes', 'hours', 'seconds']"
+          id="datePicker"
+          v-model="date"
+        />
+      </div>
+      <button class="button-shadow-border button-shadow" @click="handleSearchImages">
+        Search Images
+      </button>
+      <h2 v-if="earthDaysText" id="h2SearchInfoText"> {{ earthDaysText }}</h2>
+      <div id="images">
+        <img
+          v-for="image in images"
+          :key="image.id"
+          :src="image.img_src"
+          :title="'Taken on sol #' + image.sol"
+          class="perseveranceImage"
+        />
+      </div>
+      <button
+        class="button-shadow-border button-shadow"
+        id="moreImages"
+        @click="handleNextPage"
+        v-if="images.value"
+      >
+        More Images
+      </button>
+    </div>
+  </section>
 </template>
 
 <style>
@@ -89,13 +118,13 @@ function handleNextPage() {
   justify-content: center;
   width: 100%;
   padding: 0;
-  padding-top: .5rem;
+  padding-top: 0.5rem;
   z-index: 1;
   background-image: url(../assets/starryOverlay.png);
   background-size: 3rem 3rem;
 }
 
-div.searchSection{
+div.searchSection {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -140,8 +169,6 @@ div#images {
   background: #5b616b;
 }
 
-
-
 .button-shadow:hover {
   left: 4px;
   top: 4px;
@@ -154,7 +181,4 @@ div#images {
   border: 1px solid #c62d1f;
   box-shadow: #c62d1f 4px 4px 0px 0px;
 }
-
-
-
 </style>
